5d5de54a41d340d8c68fcdc524ffeb93cd085ee3
PHInteractive§ <dev@phide.net>
2021-03-30 11:24:20 +0200

    changed library for the OLED SSD1306 Module

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,34 @@
 #include <Arduino.h>
-#include <Wire.h>
-#include <Adafruit_SSD1306.h>
+#include <SSD1306Wire.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define SCREEN_ADDRESS 0x3D
+#define OLED_ADDR   0x3c  //Addres of the OLED-Module
+#define OLED_SDA    21  //GPIO used for I²C SDA
+#define OLED_SCL    22  //GPIO used for I²C SCL
 
-int PWM_Speed_Percent = 50;
-Adafruit_SSD1306 OLED(SCREEN_WIDTH, SCREEN_HEIGHT); //Setup OLED
+
+SSD1306Wire display(OLED_ADDR, OLED_SDA, OLED_SCL); //Setup Object to control OLED
+
+int PWM_Speed_Percent = 50; //PWM Speed used on startup/restart
+
 
 void setup() {
   Serial.begin(115200);
-  OLED.clearDisplay();  	//clear OLED in case of restart/reset
+  //OLED.clearDisplay();  	//clear OLED in case of restart/reset
   ledcSetup(0,25000, 10); //setup PWM on channel 0, 25kHz switching frequency, 10bit resolution (0-1024)
   ledcAttachPin(33, 0);   //Use GPIO33 as Output on channel 0
   pinMode(18, INPUT_PULLUP);  //pin for PWM Down
   pinMode(19, INPUT_PULLUP);  //pin for PWM Up
+  display.init();  //Initialize OLED
+  display.flipScreenVertically();  //rotate the OLED by 180° (to normal oriantation)
+  display.setFont(ArialMT_Plain_16); //set Font and Size
 }
 
 void loop() {
   ledcWrite(0, PWM_Speed_Percent*10.24);  //set PWM duty cycle in relation to the PWM_Speed_Percent int (Multiplyer = MaxResolution/100)
-  OLED.clearDisplay();
-  OLED.setCursor(0, 0);
-  OLED.print("PWM Speed:");
-  OLED.setCursor(1, 0);
-  OLED.print(PWM_Speed_Percent + "%");  //print PWM Speed in Percent
+  display.clear();  //clear OLED
+  display.drawString(0, 0, "PWM Fan Speed");       
+  display.drawString(30, 20, String(PWM_Speed_Percent) + " %");
+  display.display();  //prints the above Strings on the OLED
 
   if(digitalRead(18) == 0){
     if(PWM_Speed_Percent >= 1){ //Percentage will not go below 0%
@@ -36,5 +40,5 @@ void loop() {
       PWM_Speed_Percent++;
     }
   }
-  vTaskDelay(500 / portTICK_PERIOD_MS); //Delay is required to make the OLED more readable and Button Inputs con be used in a more precise way
+  vTaskDelay(100 / portTICK_PERIOD_MS);
 }
